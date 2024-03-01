193b3e2ced3c6f36aa1a7fd8ffdeaf73747adb2e
Matthew Tran <tran28@my.yorku.ca>
2024-03-01 16:40:46 -0500

    UPDATE: userLogin function

--- functions/UserLoginLambda/index.mjs
+++ functions/UserLoginLambda/index.mjs
@@ -1,7 +1,7 @@
 // Import necessary AWS SDK, cryptographic modules, and jsonwebtoken for JWT generation.
 import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { randomBytes, pbkdf2Sync } from 'crypto';
+import { pbkdf2Sync } from 'crypto';
 import jwt from 'jsonwebtoken';
 
 // Initialize the DynamoDB Document Client for easier interaction with DynamoDB.
@@ -10,7 +10,7 @@ const docClient = DynamoDBDocumentClient.from(dynamoDbClient);
 
 // Specify the DynamoDB table name and JWT secret key.
 const tableName = 'UserProducts';
-const jwtSecret = '5QGYWAkti8UoLDMzzOvgH_1KDLamwM8gGjRyI71CyYOKL_A02TjmZ5tmu3dfZbyz';
+const jwtSecret = process.env.JWT_SECRET;
 
 /**
  * Lambda function handler for user login.
@@ -43,7 +43,7 @@ export async function handler(event) {
         }
 
         // Password verification.
-        const [algorithm, hashFunction, iterations, salt, hash] = Item.hashedPassword.split(':');
+        const [hashFunction, iterations, salt, hash] = Item.hashedPassword.split(':');
         const derivedHash = pbkdf2Sync(password, salt, parseInt(iterations), 64, hashFunction).toString('hex');
         if (hash !== derivedHash) {
             return {
