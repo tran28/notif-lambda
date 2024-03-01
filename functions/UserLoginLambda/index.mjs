7aa897356816617784301a06065b028a013a18c8
Matthew Tran <tran28@my.yorku.ca>
2024-03-01 16:35:23 -0500

    UPDATE: remove tests

--- functions/UserLoginLambda/index.mjs
+++ functions/UserLoginLambda/index.mjs
@@ -37,7 +37,7 @@ export async function handler(event) {
         if (!Item) {
             return {
                 statusCode: 404,
-                body: JSON.stringify({ message: "User not found. Testing speed." }),
+                body: JSON.stringify({ message: "User not found." }),
                 headers: { "Access-Control-Allow-Origin": "*" }
             };
         }
